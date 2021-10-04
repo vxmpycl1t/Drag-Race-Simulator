b0465e9ee3e735fa268d44715a3875200d39b594
MyRainboww <76757647+MyRainboww@users.noreply.github.com>
2021-10-04 00:28:34 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2808,7 +2808,7 @@ function CheckForReturning() {
             return false;
         }
     }
-    if (currentCast.length == 5 && all_stars){
+    if ((currentCast.length - eliminatedCast.length) >= 1 && (currentCast.length - eliminatedCast.length) < 3 && all_stars){
         if (returningQueen == false) {
             returningQueen = true;
             return true;
@@ -2918,6 +2918,42 @@ function lsSmackD(){
 function LaLaPaRuZa(){
     var screen = new Scene();
     screen.clean();
+    screen.createHeader("Let the Lipsync Smackdown begin!!");
+    screen.createParagraph("The eliminated queens are back to compete in an epic Lipsync Smackdown and a chance to return to the competition.");
+    var smack = eliminatedCast.slice();
+    for (var i = 0; i < eliminatedCast.length - 1; i++) {
+        screen.createHorizontalLine();
+        if (smack.length == 2) {
+            var queen1 = smack[0];
+            var queen2 = smack[1];
+        }else{
+        var queen1 = smack[smack.length - 1];
+        var queen2 = smack[smack.length - 2];
+        }
+        screen.createBold(queen1.getName() + " and " + queen2.getName() + " will lipsync...");
+        screen.createBold("The time has come for you to lip-sync... for your rudemption! Good luck, and don't fuck it up.");
+        lsSong();
+        //screen.createHorizontalLine();
+        screen.createBold("I've made my decision.");
+        var lipSync = [queen1, queen2];
+        for (var i_1 = 0; i_1 < lipSync.length; i_1++) {
+            lipSync[i_1].getASLipsync();
+        }
+        lipSync.sort(function (a, b) { return (b.lipsyncScore - a.lipsyncScore); });
+        screen.createBold(lipSync[0].getName() + ", shantay you stay! " /*+ lipSync[0].lipsyncScore +")"*/);
+        screen.createBold(lipSync[1].getName() + ", sashay away. "/* + lipSync[1].lipsyncScore + ")"*/);
+        if (lipSync[0] == queen1) {
+            smack.splice(smack.indexOf(queen2), 1);
+        }else{
+            smack.splice(smack.indexOf(queen1), 1);
+        }
+    }
+    currentCast.push(lipSync[0]);
+    eliminatedCast.splice(eliminatedCast.indexOf(lipSync[0]), 1);
+}
+function LaLaPaRuZa(){
+    var screen = new Scene();
+    screen.clean();
     screen.createHeader("LaLaPaRuZa!!");
     screen.createParagraph("All of the eliminated All Stars get a chance to return to the competition. But first they have to send home a competing All Star in a shocking mid season lip sync smackdown.");
     var smack = eliminatedCast.slice();
@@ -2942,8 +2978,8 @@ function LaLaPaRuZa(){
             }
             queen1.lipsyncScore -= 4;
             lipSync_1.sort(function (a, b) { return (b.lipsyncScore - a.lipsyncScore); });
-            screen.createBold(lipSync_1[0].getName() + ", shantay you stay! (" + lipSync_1[0].lipsyncScore + ")");
-            screen.createBold(lipSync_1[1].getName() + " (" + lipSync_1[1].lipsyncScore + ") and " + lipSync_1[2].getName() + " (" + lipSync_1[2].lipsyncScore +"), you're getting the porkchop...");
+            screen.createBold(lipSync_1[0].getName() + ", shantay you stay!");
+            screen.createBold(lipSync_1[1].getName() + " and " + lipSync_1[2].getName() + ", sashay away...");
             if (queen1 == lipSync[0]) {
                 currentCast.push(lipSync_1[0]);
                 lipSync_1[0].addToTrackRecord("RTRN");
